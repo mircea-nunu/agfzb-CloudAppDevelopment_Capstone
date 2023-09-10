219ea64fb2dba1a55472fcd51c7165e8cf932316
Mircea N <nunu.mircea@gmail.com>
2023-09-10 12:49:06 -0400

    register models1
    Signed-off-by: Mircea N <nunu.mircea@gmail.com>

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,26 @@
 from django.contrib import admin
 # from .models import related models
-
+from .models import CarMake, CarModel
 
 # Register your models here.
 
 # CarModelInline class
-
+class CarModelInline(admin.StackedInline):
+    '''CarModelInline'''
+    model = CarModel
+    extra = 2
 # CarModelAdmin class
-
+class CarModelAdmin(admin.ModelAdmin):
+    '''CarModelAdmin'''
+    list_display = ['car_make', 'name', 'dealer_id', 'model_type', 'year']
+    list_filter = ['model_type', 'car_make', 'dealer_id', 'year',]
+    search_fields = ['car_make', 'name']
 # CarMakeAdmin class with CarModelInline
-
+class CarMakeAdmin(admin.ModelAdmin):
+    '''CarMakeAdmin'''
+    list_display = ['name', 'description']
+    search_fields = ['name']
+    inlines = [CarModelInline]
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
